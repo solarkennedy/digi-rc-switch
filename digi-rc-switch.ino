746eeb7ec0a3197da29683ce8cbcb66acf13da74
Kyle Anderson <kyle@xkyle.com>
2016-06-15 18:54:20 -0700

    Support 32bit rf codes

--- digi-rc-switch.ino
+++ digi-rc-switch.ino
@@ -10,6 +10,7 @@ byte in = 0;
 byte byte1 = 0;
 byte byte2 = 0;
 byte byte3 = 0;
+byte byte4 = 0;
 int next = 0;
 long code = 0;
 
@@ -17,19 +18,19 @@ long code = 0;
 // The rc-switch library requires a long int, but we can only
 // read data out of the usb 1 bytes at a time, so we must
 // compose our own long to send to rc-switch
-long composeLong(byte one, byte two, byte three){
-  return (long) one << 16 | (long) two << 8 | (long) three;
+long composeLong(byte one, byte two, byte three, byte four) {
+  return (long) four << 24 | (long) three << 16 | (long) two << 8 | (long) one;
 }
 
 void setup() {
   DigiUSB.begin();
-  mySwitch.enableTransmit(0);
+  mySwitch.enableTransmit(1);
+  mySwitch.setPulseLength(192);
 }
 
 void loop() {
   DigiUSB.refresh();
   if (DigiUSB.available() > 0) {
-
     in = DigiUSB.read();
     if (next == 0) {
       if (in == 115) {
@@ -53,15 +54,24 @@ void loop() {
       byte3 = in;
       DigiUSB.print("byte3 ");
       DigiUSB.println(in, HEX);
-      code = composeLong(byte1, byte2, byte3);
+      next = 4;
+    }
+    else if (next == 4) {
+      byte4 = in;
+      DigiUSB.print("byte4 ");
+      DigiUSB.println(in, HEX);
+      code = composeLong(byte1, byte2, byte3, byte4);
       DigiUSB.print("Sending Code:");
-      DigiUSB.println(code, HEX);
+      DigiUSB.print(code);
+      DigiUSB.print(" (hex: ");
+      DigiUSB.print(code, HEX);
+      DigiUSB.print(")...");
       mySwitch.send(code, 24);
+      DigiUSB.println("Done");
       next = 0;
     } else {
       DigiUSB.print("Else Got:");
       DigiUSB.println(in, HEX);
     }
   }
-
 }
--- python-driver/digi-rc-switch.py
+++ python-driver/digi-rc-switch.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python
-import usb
 import sys
+import struct
 
 sys.path.append("..")
 from arduino.usbdevice import ArduinoUsbDevice
 
 
+def codeToBytes(code):
+    return struct.unpack("4B", struct.pack("I", code))
+
+
 def sendCode(code):
     theDevice.write(ord("s"))
-    # TODO breakup into bytes
-    theDevice.write(code)
+    print "Sending %s" % str(code)
+    for byte in codeToBytes(code):
+        print "  Sending byte %s" % hex(byte)
+        theDevice.write(byte)
 
-
-def validateCode(code):
-    # TODO: validate the code can fit into a 24bit long
-    pass
-
-
-def printHelp():
-    pass
 
 if __name__ == "__main__":
-    theDevice = ArduinoUsbDevice(idVendor=0x16c0, idProduct=0x05df)
-    print "Found: 0x%04x 0x%04x %s %s" % (
-        theDevice.idVendor, 
-        theDevice.idProduct,
-        theDevice.productName,
-        theDevice.manufacturer
-    )
-
     if len(sys.argv) != 2:
-        printHelp
+        print "Usage: %s CODE"
+        print "CODE must be a 32 bit int."
     else:
-        code=sys.argv[1]
-        validateCode(code)
+        theDevice = ArduinoUsbDevice(idVendor=0x16c0, idProduct=0x05df)
+        print "Found: 0x%04x 0x%04x %s %s" % (
+            theDevice.idVendor,
+            theDevice.idProduct,
+            theDevice.productName,
+            theDevice.manufacturer
+        )
+        code=int(sys.argv[1])
         sendCode(code)
