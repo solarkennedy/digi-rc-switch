0093392f4c3218eb3d3f1aae42fa7fc8067cb817
Kyle Anderson <kwa@yelp.com>
2016-11-19 14:30:20 -0800

    toggled off debug printing

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 Code to use a Digispark (ATtiny85) to send rc-switch RF codes over a 433mhz
 transmitter via USB.
 
+## Operation
+
+When operating, the device shows up as a serial port. Sending the 4 bytes (long)
+to the serial port initiate the RF code transmission.
+
 ## Hardware
 
 * [433mhz FSK Transmitter](https://www.sparkfun.com/products/10534)
@@ -16,5 +21,8 @@ DATA -------- PIN1
 ```
 
 ## Software
+
+Uses the [rc-switch](https://github.com/sui77/rc-switch) library to send
+RF codes.
 
 See the `python-driver` directory for a python script to send RF codes
--- digi-rc-switch.ino
+++ digi-rc-switch.ino
@@ -12,6 +12,10 @@ long code = 0;
 long last_time = 0;
 
 
+// The debug print code interferes with the stability
+// of the usb thing, the attiny can't print out reliably.
+#define DEBUG 0
+
 // The rc-switch library requires a long int, but we can only
 // read data out of the usb 1 bytes at a time, so we must
 // compose our own long to send to rc-switch
@@ -30,7 +34,9 @@ void loop() {
 
   if (SerialUSB.available()) {
     if (last_time <= millis() - 1000) {
+      #ifdef DEBUG
       SerialUSB.println("Start");
+      #endif
       next = 1;
       last_time = millis();
     }
@@ -38,39 +44,53 @@ void loop() {
 
     if (next == 1) {
       byte1 = in;
+      #ifdef DEBUG
       SerialUSB.print(F("byte1 "));
       SerialUSB.println(in, HEX);
+      #endif
       next = 2;
     }
     else if (next == 2) {
       byte2 = in;
+      #ifdef DEBUG
       SerialUSB.print(F("byte2 "));
       SerialUSB.println(in, HEX);
+      #endif
       next = 3;
     }
     else if (next == 3) {
       byte3 = in;
+      #ifdef DEBUG
       SerialUSB.print(F("byte3 "));
       SerialUSB.println(in, HEX);
+      #endif
       next = 4;
     }
     else if (next == 4) {
       byte4 = in;
+      #ifdef DEBUG
       SerialUSB.print(F("byte4 "));
       SerialUSB.println(in, HEX);
+      #endif
       code = composeLong(byte1, byte2, byte3, byte4);
+      #ifdef DEBUG
       SerialUSB.print(F("Sending Code:"));
       SerialUSB.print(code);
       SerialUSB.print(F(" (hex: "));
       SerialUSB.print(code, HEX);
       SerialUSB.print(F(")..."));
+      #endif
       mySwitch.send(code, 24);
+      #ifdef DEBUG
       SerialUSB.println(F("Done"));
       SerialUSB.println("");
+      #endif
       next = 0;
+    #ifdef DEBUG
     } else {
       SerialUSB.print(F("Else Got:"));
       SerialUSB.println(in, HEX);
+    #endif
     }
   }
   SerialUSB.refresh();
